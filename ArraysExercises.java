7e75479dd189191a39e4829540398e8d8e08a8d1
Daniel Carlsson <unix555@gmail.com>
2023-11-27 19:46:53 +0100

    Task 3 not completed yet

--- ArraysExercises.java
+++ ArraysExercises.java
@@ -2,6 +2,7 @@ package se.lexicon.Arrays;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 public class ArraysExercises {
 
@@ -9,6 +10,7 @@ public class ArraysExercises {
 
         ex1();
         indexOf();
+        ex3();
 
     }
 
@@ -18,16 +20,25 @@ public class ArraysExercises {
 
             for (int i = 0; i < numbers.length; i++) {
                 System.out.println(numbers[i]);
+
             }
         }
             public static void indexOf(){
+                System.out.println("--------");
                 int[] numbers2 = {3, 4, 5, 12, 9};
                 Arrays.sort(numbers2);
                 int position = Arrays.binarySearch(numbers2, 5);
                 System.out.println("Index position of number 5 is: " +position);
-
+                System.out.println("_______");
 
             }
 
+            public static void ex3() {
+                String[] names = {"Paris,London, New York, Stockholm"};
+                Arrays.sort(names);
+                for (String name : names) {
+                    System.out.println(name);
+                }
 
+    }
         }

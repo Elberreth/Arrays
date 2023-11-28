9cfda3dabab2498d8e73f506eddf0d63f78d51cc
Daniel Carlsson <unix555@gmail.com>
2023-11-28 11:09:46 +0100

    new task added

--- ArraysExercises.java
+++ ArraysExercises.java
@@ -11,7 +11,8 @@ public class ArraysExercises {
         ex1();
         indexOf();
         ex3();
-
+        ex4();
+        ex5();
     }
 
         public static void ex1 () {
@@ -34,11 +35,48 @@ public class ArraysExercises {
             }
 
             public static void ex3() {
+
                 String[] names = {"Paris,London, New York, Stockholm"};
                 Arrays.sort(names);
+
                 for (String name : names) {
-                    System.out.println(name);
+                    System.out.println("String Array : " +name);
                 }
 
     }
+
+            public static void ex4() {
+
+
+                System.out.println("-----------");
+                int[] firstArray = {1, 15, 20};
+
+                // Create a second array with the same length as the first array
+                int[] secondArray = new int[firstArray.length];
+
+                // Copy elements from the first array to the second array
+                for (int i = 0; i < firstArray.length; i++) {
+                    secondArray[i] = firstArray[i];
+                }
+
+                // Print elements from the first array
+                System.out.print("Elements from first array: ");
+                for (int element : firstArray) {
+                    System.out.print(element + " ");
+                }
+
+                // Print elements from the second array
+                System.out.print("\nElements from second array: ");
+                for (int element : secondArray) {
+                    System.out.print(element + " ");
+                }
+
+
+            }
+
+
+            public static void ex5() {
+            
+
+            }
         }
